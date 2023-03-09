8f08f806b06e4d30c9c108422c041ab6913c7a53
IT22557124 <127375583+IT22557124@users.noreply.github.com>
2023-03-09 06:56:59 +0000

    tutorial 1

--- .ccls-cache/@home@runner@tutorial-01-IT22557124/Tute04.c
+++ .ccls-cache/@home@runner@tutorial-01-IT22557124/Tute04.c
@@ -5,9 +5,6 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
-int minimum(int no1,int no2);
-int maximum(int no1,int no2);
-int multiply(int no1,int no2);
 
 int main() {
    int no1, no2;
@@ -33,7 +30,7 @@ int minimum(int no1,int no2)
     return no1;
   }
 }
-int maximum(int no1,int no2)
+int maxium(int no1,int no2)
 {
   if(no1>no2)
   {
